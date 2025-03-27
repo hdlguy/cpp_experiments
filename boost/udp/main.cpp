274d9f91e0d2b5aa6bdc580b2261ec315162cd9f
hdlguy <hdlguy@hdlguy.com>
2025-03-27 12:58:02 -0400

    sending and receiving udp packets from fpga using boost library

--- boost/udp/main.cpp
+++ boost/udp/main.cpp
@@ -1,26 +1,49 @@
 #include <boost/asio.hpp>
 #include <iostream>
+#include <stdio.h>
+
+#define     UDP_STAT_CON        9
+#define     UDP_STAT_REQ        10
 
 using boost::asio::ip::udp;
-
-void udp_client(const std::string& server_ip, int port)
-{
-    boost::asio::io_service io_service;
-    udp::socket socket(io_service);
-    udp::endpoint receiver_endpoint(boost::asio::ip::address::from_string(server_ip), port);
-
-    std::string message = "Hello, UDP Server!";
-    socket.open(udp::v4());
-
-    socket.send_to(boost::asio::buffer(message), receiver_endpoint);
-    std::cout << "Sent message to server: " << message << std::endl;
-}
 
 int main()
 {
     std::string server_ip = "16.0.0.128";  // Replace with the server's IP
     int port = 1234;
-    udp_client(server_ip, port);
+
+    // setup tx socket
+    boost::asio::io_service tx_io_service;
+    udp::socket tx_socket(tx_io_service);
+    udp::endpoint device_endpoint(boost::asio::ip::address::from_string(server_ip), port);
+    tx_socket.open(udp::v4());
+
+    // setup rx socket
+    boost::asio::io_service rx_io_service;
+    udp::socket rx_socket(rx_io_service, udp::endpoint(udp::v4(), port));
+    std::cout << "UDP server listening on port " << port << "..." << std::endl;
+
+
+    // send packet
+    char txbuf[2048];
+    txbuf[0] = 0xaa; txbuf[1] = 0xbb; txbuf[2] = 0xcc; txbuf[3] = UDP_STAT_REQ;
+    tx_socket.send_to(boost::asio::buffer(std::string(txbuf,4)), device_endpoint);
+
+    // receive packets
+    char rxbuf[1024];
+    udp::endpoint remote_endpoint;
+    uint8_t fpga_source;
+    do {
+
+        size_t length = rx_socket.receive_from(boost::asio::buffer(rxbuf), remote_endpoint);
+        fpga_source = rxbuf[3];
+
+    } while (fpga_source != UDP_STAT_CON);
+
+
+    printf("fpga_source = 0x%02x", fpga_source);
+    std::cout << " from " << remote_endpoint << std::endl;
+
     return 0;
 }
 
