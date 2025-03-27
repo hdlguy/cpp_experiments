dd42419e4ce4c93c473f384f5d8129e17f42f589
hdlguy <hdlguy@hdlguy.com>
2025-03-27 16:22:10 -0400

    listing interfaces

--- boost/ifaces/main.cpp
+++ boost/ifaces/main.cpp
@@ -1,32 +1,64 @@
-#include <boost/asio.hpp>
 #include <iostream>
+#include <boost/asio.hpp>
 
-void list_network_interfaces()
-{
-    try
-    {
-        boost::asio::io_service io_service;
-        boost::asio::ip::tcp::resolver resolver(io_service);
-        boost::asio::ip::tcp::resolver::query query(boost::asio::ip::host_name(), "");
-        boost::asio::ip::tcp::resolver::iterator it = resolver.resolve(query);
-        
-        std::cout << "Network Interfaces:\n";
+#ifdef __linux__
+    #include <ifaddrs.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+#elif _WIN32
+    #include <winsock2.h>
+    #include <iphlpapi.h>
+    #pragma comment(lib, "iphlpapi.lib")
+#endif
 
-        while (it != boost::asio::ip::tcp::resolver::iterator())
-        {
-            boost::asio::ip::tcp::endpoint endpoint = *it++;
-            std::cout << " - " << endpoint.address().to_string() << std::endl;
+
+void LoadNetworkInterfaces() {
+    struct ifaddrs* ifaddr;
+    if (getifaddrs(&ifaddr) == -1) {
+        //wxMessageBox("Error fetching interfaces", "Error", wxICON_ERROR);
+        std::cout << "Error Fetching interfaces\n";
+        return;
+    }
+
+    for (struct ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr && ifa->ifa_addr->sa_family == AF_INET && (ifa->ifa_flags & IFF_UP)) {
+            char ip[INET_ADDRSTRLEN] = {0};
+            struct sockaddr_in* sa = (struct sockaddr_in*)ifa->ifa_addr;
+            inet_ntop(AF_INET, &sa->sin_addr, ip, INET_ADDRSTRLEN);
+            std::cout << ifa->ifa_name << "(" << ip << ")\n";
+            //wxString ifaceEntry = wxString::Format("%s (%s)", ifa->ifa_name, ip);
+            //interfaceChoice->Append(ifaceEntry);
         }
     }
-    catch (std::exception& e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+    freeifaddrs(ifaddr);
 }
+
 
 int main()
 {
-    list_network_interfaces();
+    //list_network_interfaces();
+    LoadNetworkInterfaces();
+
     return 0;
 }
 
+/*
+    void LoadNetworkInterfaces() {
+        struct ifaddrs* ifaddr;
+        if (getifaddrs(&ifaddr) == -1) {
+            wxMessageBox("Error fetching interfaces", "Error", wxICON_ERROR);
+            return;
+        }
+
+        for (struct ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+            if (ifa->ifa_addr && ifa->ifa_addr->sa_family == AF_INET && (ifa->ifa_flags & IFF_UP)) {
+                char ip[INET_ADDRSTRLEN] = {0};
+                struct sockaddr_in* sa = (struct sockaddr_in*)ifa->ifa_addr;
+                inet_ntop(AF_INET, &sa->sin_addr, ip, INET_ADDRSTRLEN);
+                wxString ifaceEntry = wxString::Format("%s (%s)", ifa->ifa_name, ip);
+                interfaceChoice->Append(ifaceEntry);
+            }
+        }
+        freeifaddrs(ifaddr);
+    }
+*/
