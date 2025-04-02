a13742d7b90eed4ce9461683cbdc6c50236e243c
hdlguy <hdlguy@wherever.com>
2025-04-02 12:51:26 -0400

    working networking techniques for windows

--- boost/udp/main.cpp
+++ boost/udp/main.cpp
@@ -1,6 +1,7 @@
 #include <boost/asio.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <cstdio>
 
 #define     UDP_STAT_CON        9
 #define     UDP_STAT_REQ        10
@@ -15,12 +16,20 @@ void SetIPAddress(const std::string& interface, const std::string& ipAddress);
 int main()
 {
     // network settings
-    std::string network_interface = "enx94103eb7e201";
+    //std::string network_interface = "enx94103eb7e201";
+    //std::string windows_interface = "Ethernet 2";
+    std::string network_interface;
     std::string pc_ip     = "16.0.0.200";  
     std::string device_ip = "16.0.0.128"; 
     int port = 1234;
 
-    // Set IP address on network interface 
+    // Set IP address on network interface     
+#ifdef __linux__
+    network_interface = "enx94103eb7e201";
+#elif _WIN32
+    network_interface = "Ethernet 2";
+#endif
+
     //SetIPAddress(network_interface, pc_ip);
 
     // setup tx socket
@@ -35,15 +44,17 @@ int main()
     std::cout << "UDP server listening on port " << port << "..." << std::endl;
 
     // send packet
-    char txbuf[BUF_LEN];
+    uint8_t txbuf[BUF_LEN];
     txbuf[0] = 0xaa; txbuf[1] = 0xbb; txbuf[2] = 0xcc; txbuf[3] = UDP_STAT_REQ;
-    tx_socket.send_to(boost::asio::buffer(std::string(txbuf,4)), device_endpoint);
+    std::cout << "sending command packet\n";
+    tx_socket.send_to(boost::asio::buffer(std::string((char *)txbuf,4)), device_endpoint);
 
     // receive packets
     char rxbuf[BUF_LEN];
     uint32_t* rxregbuf = (uint32_t *)rxbuf;
     udp::endpoint remote_endpoint;
     uint8_t fpga_source;
+    std::cout << "waiting for response packet\n";
     do {
         size_t length = rx_socket.receive_from(boost::asio::buffer(rxbuf), remote_endpoint);
         fpga_source = rxbuf[3];
@@ -56,6 +67,8 @@ int main()
     printf("fpga_source = 0x%02x, fpga_id = 0x%08x, fpga_version = 0x%08x", fpga_source, fpga_id, fpga_version);
     std::cout << " from " << remote_endpoint << std::endl;
 
+    std::getchar();
+
     return 0;
 }
 
@@ -66,6 +79,7 @@ void SetIPAddress(const std::string& interface, const std::string& ipAddress) {
         command = "sudo ifconfig " + interface + " " + ipAddress;
     #elif _WIN32
         command = "netsh interface ip set address name=\"" + interface + "\" static " + ipAddress + " 255.255.255.0";
+        //command = "netsh interface ip set address name = \"Ethernet 2\" static 16.0.0.200 255.255.255.0";
     #else
         std::cerr << "Unsupported platform" << std::endl;
         return;
---
+++ boost/win_net/win_net/win_net.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <winsock2.h>
+#include <iphlpapi.h>
+#pragma comment(lib, "iphlpapi.lib")
+
+void PrintNetworkInterfaces() {
+    ULONG bufferSize = 0;
+    GetAdaptersAddresses(AF_INET, 0, NULL, NULL, &bufferSize);
+
+    PIP_ADAPTER_ADDRESSES adapterAddresses = (PIP_ADAPTER_ADDRESSES)malloc(bufferSize);
+    if (GetAdaptersAddresses(AF_INET, 0, NULL, adapterAddresses, &bufferSize) == NO_ERROR) {
+        for (PIP_ADAPTER_ADDRESSES adapter = adapterAddresses; adapter; adapter = adapter->Next) {
+            std::wcout << L"Interface Name: " << adapter->FriendlyName << ",  " << adapter->Description << std::endl;
+        }
+    }
+    free(adapterAddresses);
+}
+
+int main() {
+    PrintNetworkInterfaces();
+    return 0;
+}
